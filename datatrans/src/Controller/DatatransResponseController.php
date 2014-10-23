6510b9630ef8477f92674259686280417f6360ae
anushka-mp <anushka.mp@live.com>
2014-10-23 16:59:40 +0200

    moved mapErrorCode into DatatransHelper

--- datatrans/src/Controller/DatatransResponseController.php
+++ datatrans/src/Controller/DatatransResponseController.php
@@ -40,7 +40,7 @@ class DatatransResponseController {
 
       // If Datatrans returns error status.
       if ($post_data['status'] == 'error') {
-        throw new \Exception($this->mapErrorCode($post_data['errorCode']));
+        throw new \Exception(DatatransHelper::mapErrorCode($post_data['errorCode']));
       }
 
       // This is the internal guaranteed configuration that is to be considered
@@ -152,112 +152,7 @@ class DatatransResponseController {
     $payment->getPaymentType()->resumeContext();
   }
 
-  /**
-   * Datatrans error code mapping.
-   *
-   * @param $code
-   *   Provide error code from Datatrans callback.
-   * @return bool|FALSE|mixed|string
-   *   Returns error message.
-   *
-   * @TODO: Move this method somewhere else.
-   */
-  function mapErrorCode($code) {
-    switch ($code) {
-      case '1001':
-        $message = t('Datrans transaction failed: missing required parameter.');
-        break;
 
-      case '1002':
-        $message = t('Datrans transaction failed: invalid parameter format.');
-        break;
-
-      case '1003':
-        $message = t('Datatrans transaction failed: value of parameter not found.');
-        break;
-
-      case '1004':
-      case '1400':
-        $message = t('Datatrans transaction failed: invalid card number.');
-        break;
-
-      case '1007':
-        $message = t('Datatrans transaction failed: access denied by sign control/parameter sign invalid.');
-        break;
-
-      case '1008':
-        $message = t('Datatrans transaction failed: merchant disabled by Datatrans.');
-        break;
-
-      case '1401':
-        $message = t('Datatrans transaction failed: invalid expiration date.');
-        break;
-
-      case '1402':
-      case '1404':
-        $message = t('Datatrans transaction failed: card expired or blocked.');
-        break;
-
-      case '1403':
-        $message = t('Datatrans transaction failed: transaction declined by card issuer.');
-        break;
-
-      case '1405':
-        $message = t('Datatrans transaction failed: amount exceeded.');
-        break;
-
-      case '3000':
-      case '3001':
-      case '3002':
-      case '3003':
-      case '3004':
-      case '3005':
-      case '3006':
-      case '3011':
-      case '3012':
-      case '3013':
-      case '3014':
-      case '3015':
-      case '3016':
-        $message = t('Datatrans transaction failed: denied by fraud management.');
-        break;
-
-      case '3031':
-        $message = t('Datatrans transaction failed: declined due to response code 02.');
-        break;
-
-      case '3041':
-        $message = t('Datatrans transaction failed: Declined due to post error/post URL check failed.');
-        break;
-
-      case '10412':
-        $message = t('Datatrans transaction failed: PayPal duplicate error.');
-        break;
-
-      case '-885':
-      case '-886':
-        $message = t('Datatrans transaction failed: CC-alias update/insert error.');
-        break;
-
-      case '-887':
-        $message = t('Datatrans transaction failed: CC-alias does not match with cardno.');
-        break;
-
-      case '-888':
-        $message = t('Datatrans transaction failed: CC-alias not found.');
-        break;
-
-      case '-900':
-        $message = t('Datatrans transaction failed: CC-alias service not enabled.');
-        break;
-
-      default:
-        $message = t('Datatrans transaction failed: undefined error.');
-        break;
-    }
-
-    return $message;
-  }
 
   /**
    * Sets payments configuration data if present.
--- datatrans/src/DatatransHelper.php
+++ datatrans/src/DatatransHelper.php
@@ -47,4 +47,111 @@ class DatatransHelper {
     $hmac_data = $merchant_id . $amount . $currency . $identifier;
     return hash_hmac('md5', $hmac_data, pack('H*', $hmac_key));
   }
+
+  /**
+   * Datatrans error code mapping.
+   *
+   * @param $code
+   *   Provide error code from Datatrans callback.
+   * @return bool|FALSE|mixed|string
+   *   Returns error message.
+   *
+   * @TODO: Move this method somewhere else.
+   */
+  public static function mapErrorCode($code) {
+    switch ($code) {
+      case '1001':
+        $message = t('Datrans transaction failed: missing required parameter.');
+        break;
+
+      case '1002':
+        $message = t('Datrans transaction failed: invalid parameter format.');
+        break;
+
+      case '1003':
+        $message = t('Datatrans transaction failed: value of parameter not found.');
+        break;
+
+      case '1004':
+      case '1400':
+        $message = t('Datatrans transaction failed: invalid card number.');
+        break;
+
+      case '1007':
+        $message = t('Datatrans transaction failed: access denied by sign control/parameter sign invalid.');
+        break;
+
+      case '1008':
+        $message = t('Datatrans transaction failed: merchant disabled by Datatrans.');
+        break;
+
+      case '1401':
+        $message = t('Datatrans transaction failed: invalid expiration date.');
+        break;
+
+      case '1402':
+      case '1404':
+        $message = t('Datatrans transaction failed: card expired or blocked.');
+        break;
+
+      case '1403':
+        $message = t('Datatrans transaction failed: transaction declined by card issuer.');
+        break;
+
+      case '1405':
+        $message = t('Datatrans transaction failed: amount exceeded.');
+        break;
+
+      case '3000':
+      case '3001':
+      case '3002':
+      case '3003':
+      case '3004':
+      case '3005':
+      case '3006':
+      case '3011':
+      case '3012':
+      case '3013':
+      case '3014':
+      case '3015':
+      case '3016':
+        $message = t('Datatrans transaction failed: denied by fraud management.');
+        break;
+
+      case '3031':
+        $message = t('Datatrans transaction failed: declined due to response code 02.');
+        break;
+
+      case '3041':
+        $message = t('Datatrans transaction failed: Declined due to post error/post URL check failed.');
+        break;
+
+      case '10412':
+        $message = t('Datatrans transaction failed: PayPal duplicate error.');
+        break;
+
+      case '-885':
+      case '-886':
+        $message = t('Datatrans transaction failed: CC-alias update/insert error.');
+        break;
+
+      case '-887':
+        $message = t('Datatrans transaction failed: CC-alias does not match with cardno.');
+        break;
+
+      case '-888':
+        $message = t('Datatrans transaction failed: CC-alias not found.');
+        break;
+
+      case '-900':
+        $message = t('Datatrans transaction failed: CC-alias service not enabled.');
+        break;
+
+      default:
+        $message = t('Datatrans transaction failed: undefined error.');
+        break;
+    }
+
+    return $message;
+  }
 }
